30468ec034e31391eebd180dfbf8e032f9bd76b7
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-24 19:09:23 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,17 +1,22 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total(total)
   # code #display_card_total here
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "would you like to hit or stay"
+  input = gets.chomp
 end
 
 def get_user_input
